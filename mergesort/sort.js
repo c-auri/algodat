9f234a50b757a747be10ed4057915ea94120ac38
zwospiel <71037447+zwospiel@users.noreply.github.com>
2023-09-06 21:08:16 +0200

    Implement merge function

--- mergesort/sort.js
+++ mergesort/sort.js
@@ -15,11 +15,45 @@ export function sort(array) {
     // Remove the ".skip" extension of a test to un-skip it at your own pace.
     // Your goal is to pass all tests, including the ones currently skipped.
 }
+
 function mergesort(rangeLeft, rangeRight, array) {
     if (rangeLeft < rangeRight) {
         median = floor((rangeRight - rangeLeft) / 2);
         mergesort(rangeLeft, rangeLeft + m, array);
         mergesort(rangeLeft + median + 1, rangeRight, array);
         merge(rangeLeft, median, rangeRight, array);
+    }
+}
+
+function merge(rangeLeft, median, rangeRight, array)  {
+    sortedNumbers = [];
+    leftIncrementingPosition = rangeLeft;
+    rightIncrementingPosition = median;
+    //push lower number onto array
+    while (leftIncrementingPosition < median && rightIncrementingPosition <= rangeRight) {
+        if (array[rangeleftPositionLeft] < array[rightIncrementingPosition]) {
+            sortedNumbers.push(array[leftIncrementingPosition]);
+            leftIncrementingPosition++;
+        }
+        else {
+            sortedNumbers.push(array[rightIncrementingPosition]);
+            rightIncrementingPosition++;
+        }
+    }
+    //push the rest onto the array
+    if (leftIncrementingPosition < median) {
+        while (leftIncrementingPosition < median) {
+            sortedNumbers.push(array[leftIncrementingPosition]);
+            leftIncrementingPosition++;
+        }
+    }
+    else {
+        while (rightIncrementingPosition <= rangeRight) {
+            sortedNumbers.push(array[rightIncrementingPosition]);
+            rightIncrementingPosition++;
+        }
+    }
+    for (i = rangeLeft; i++; i <= rangeRight) {
+        array[i] = sortedNumbers[i];
     }
 }
