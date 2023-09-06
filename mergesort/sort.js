b58a5013f4c832a131666a630a76c45610f01fd1
zwospiel <71037447+zwospiel@users.noreply.github.com>
2023-09-06 15:52:10 +0200

    Add mergesort function

--- mergesort/sort.js
+++ mergesort/sort.js
@@ -4,6 +4,9 @@
  * @param {number[]} array 
  */
 export function sort(array) {
+    rangeLeft = 0;
+    rangeRight = array.length-1;
+    mergesort(rangeLeft, rangeRight, array);
     // TODO:
     // Implement the function using mergesort.
     // You may use private helper functions in this file.
@@ -11,4 +14,12 @@ export function sort(array) {
     // To not flood the console, most tests are skipped initially.
     // Remove the ".skip" extension of a test to un-skip it at your own pace.
     // Your goal is to pass all tests, including the ones currently skipped.
+}
+function mergesort(rangeLeft, rangeRight, array) {
+    if (rangeLeft < rangeRight) {
+        median = floor((rangeRight - rangeLeft) / 2);
+        mergesort(rangeLeft, rangeLeft + m, array);
+        mergesort(rangeLeft + median + 1, rangeRight, array);
+        merge(rangeLeft, median, rangeRight, array);
+    }
 }
