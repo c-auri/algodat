8c9a5bf84423bd9db3234a8e1ffd56b4d471bc27
c-auri <43008483+c-auri@users.noreply.github.com>
2023-09-11 00:51:53 +0200

    Add maxsubarray problem

---
+++ src/maxsubarray/MaxSubArray.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the subarray of the given numbers with the largest sum.
+ * The empty array is allowed as a solution.
+ * @param {number[]} numbers
+ * @returns {number}
+ */
+export function getMaxSubarray(numbers) {
+    // TODO: implement a solution in linear time
+    // Hint: You will probably need the Array.slice function:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+}
---
+++ src/maxsubarray/MaxSubArray.test.js
@@ -0,0 +1,35 @@
+import { getMaxSubarray } from "./MaxSubArray";
+
+
+describe("maxSubArray", () => {
+    describe("returns the empty array", () => {
+        test.skip("for the empty array", () => {
+            expect(getMaxSubarray([])).toEqual([])
+        })
+        test.skip("for an array with a single negative element", () => {
+            expect(getMaxSubarray([ -1 ])).toEqual([])
+        })
+        test.skip("for an array with only negative elements", () => {
+            expect(getMaxSubarray([ -1, -5, -7 ])).toEqual([])
+        })
+    })
+    describe("returns the input array", () => {
+        test.skip("for an array with a single positive element", () => {
+            expect(getMaxSubarray([ 1 ])).toEqual([ 1 ])
+        })
+        test.skip("for an array with only positive elements", () => {
+            expect(getMaxSubarray([ 1, 7, 5 ])).toEqual([ 1, 7, 5 ])
+        })
+        test.skip("for an array with a single small negative number in the middle", () => {
+            const array = [ 5, 4, -1, 7, 8 ]
+            expect(getMaxSubarray(array)).toEqual(array)
+        })
+    })
+    describe("returns the correct maximum subarray", () => {
+        test.skip("for  problem with a true subarray as solution", () => {
+            const array = [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ]
+            const solution = [ 4, -1, 2, 1 ]
+            expect(getMaxSubarray(array)).toEqual(solution)
+        })
+    })
+})
