1369454d2dab363d6c8758779e90d1ad59053400
c-auri <43008483+c-auri@users.noreply.github.com>
2023-09-11 01:48:03 +0200

    Add maxsubarray test

--- src/maxsubarray/MaxSubarray.test.js
+++ src/maxsubarray/MaxSubarray.test.js
@@ -3,32 +3,39 @@ import { getMaxSubarray } from "./MaxSubarray";
 
 describe("getMaxSubarray", () => {
     describe("returns the empty array", () => {
-        test.skip("for the empty array", () => {
+        test("for the empty array", () => {
             expect(getMaxSubarray([])).toEqual([])
         })
-        test.skip("for an array with a single negative element", () => {
+        test("for an array with a single negative element", () => {
             expect(getMaxSubarray([ -1 ])).toEqual([])
         })
-        test.skip("for an array with only negative elements", () => {
+        test("for an array with only negative elements", () => {
             expect(getMaxSubarray([ -1, -5, -7 ])).toEqual([])
         })
     })
     describe("returns the input array", () => {
-        test.skip("for an array with a single positive element", () => {
+        test("for an array with a single positive element", () => {
             expect(getMaxSubarray([ 1 ])).toEqual([ 1 ])
         })
-        test.skip("for an array with only positive elements", () => {
+        test("for an array with only positive elements", () => {
             expect(getMaxSubarray([ 1, 7, 5 ])).toEqual([ 1, 7, 5 ])
         })
-        test.skip("for an array with a single small negative number in the middle", () => {
+        test("for an array with a single small negative number in the middle", () => {
             const array = [ 5, 4, -1, 7, 8 ]
             expect(getMaxSubarray(array)).toEqual(array)
         })
     })
     describe("returns the correct maximum subarray", () => {
-        test.skip("for a problem with a non-trivial subarray as solution", () => {
+        test("for a problem with a non-trivial subarray as solution", () => {
             const array = [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ]
             const solution = [ 4, -1, 2, 1 ]
+            expect(getMaxSubarray(array)).toEqual(solution)
+        })
+    })
+    describe("returns the first solution", () => {
+        test("when there are two subarrays with the maximum sum", () => {
+            const array = [ 4, -1, 3, -5, 2, -7, 2, -1, 5 ]
+            const solution = [ 4, -1, 3 ]
             expect(getMaxSubarray(array)).toEqual(solution)
         })
     })
