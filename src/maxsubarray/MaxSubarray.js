4bf28c72db6005b0118ef42153ec3c66e9631959
zwospiel <71037447+zwospiel@users.noreply.github.com>
2023-09-18 16:34:30 +0200

    Implement getMaxSubaray Algorithm

--- src/maxsubarray/MaxSubarray.js
+++ src/maxsubarray/MaxSubarray.js
@@ -13,4 +13,24 @@ export function getMaxSubarray(numbers, allowEmptySolution = true) {
     if (!allowEmptySolution && numbers.length === 0) {
         throw new Error("Input array must not be empty.")
     }
+    let currentSum = numbers[0];
+    let biggestSum = numbers[0];
+    let biggestArray = [ numbers[0] ];
+    let i = 0;
+    let j = 0;
+    for (let k = 1; k++; k <= numbers.length - 1) {
+        if (currentSum + numbers[k] < numbers[k]) {
+            currentSum = numbers[k];
+            i = k;
+            j = k;
+        } else {
+            currentSum = currentSum + numbers[k];
+            j++;
+        }
+        if (biggestSum < currentSum) {
+            biggestSum = currentSum;
+            biggestArray = numbers.slice(i, j);
+        }
+    }
+    return biggestArray;
 }
