8484099e4dc8cee5c2d45741b1bd5667ca990088
c-auri <43008483+c-auri@users.noreply.github.com>
2023-09-19 22:51:43 +0200

    Solve mergesort

--- src/mergesort/Mergesort.js
+++ src/mergesort/Mergesort.js
@@ -4,51 +4,36 @@
  * @param {number[]} array
  */
 export function sort(array) {
-    const rangeLeft = 0;
-    const rangeRight = array.length - 1;
-    mergesort(rangeLeft, rangeRight, array);
-    return array;
+    if (array.length <= 1) {
+        return array
+    }
+
+    const { left, right } = split(array)
+
+    return merge(sort(left), sort(right))
 }
 
-function mergesort(rangeLeft, rangeRight, array) {
-    if (rangeLeft < rangeRight) {
-        const median = Math.floor((rangeRight - rangeLeft) / 2);
-        mergesort(rangeLeft, rangeLeft + median, array);
-        mergesort(rangeLeft + median + 1, rangeRight, array);
-        merge(rangeLeft, rangeLeft + median, rangeRight, array);
+function split(array) {
+    const middleIndex = Math.round(array.length / 2)
+
+    return {
+        left: array.slice(0, middleIndex),
+        right: array.slice(middleIndex)
     }
 }
 
-function merge(rangeLeft, median, rangeRight, array) {
-    const sortedNumbers = [];
-    let leftIncrementingPosition = rangeLeft;
-    let rightIncrementingPosition = median + 1;
-    //push lower number onto array
-    while ((leftIncrementingPosition <= median) && (rightIncrementingPosition <= rangeRight)) {
-        if (array[leftIncrementingPosition] < array[rightIncrementingPosition]) {
-            sortedNumbers.push(array[leftIncrementingPosition]);
-            leftIncrementingPosition++;
+function merge(left, right) {
+    const result = []
+    let i = 0
+    let j = 0
+
+    while (i < left.length || j < right.length) {
+        if (j === right.length || left[i] <= right[j]) {
+            result.push(left[i++])
         } else {
-            sortedNumbers.push(array[rightIncrementingPosition]);
-            rightIncrementingPosition++;
+            result.push(right[j++])
         }
     }
 
-    //push the rest onto the array
-    if (leftIncrementingPosition <= median) {
-        while (leftIncrementingPosition <= median) {
-            sortedNumbers.push(array[leftIncrementingPosition]);
-            leftIncrementingPosition++;
-        }
-    } else {
-        while (rightIncrementingPosition <= rangeRight) {
-            sortedNumbers.push(array[rightIncrementingPosition]);
-            rightIncrementingPosition++;
-        }
-    }
-    const changeRange = rangeRight - rangeLeft;
-    for (let i = 0; i <= changeRange; i++) {
-        array[rangeLeft] = sortedNumbers[i];
-        rangeLeft++;
-    }
+    return result
 }
